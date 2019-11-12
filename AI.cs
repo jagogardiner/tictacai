e2a52e7fd538ef82980d4a9bbcf5760a4252326e
nysascape <nysadev@raphielgang.org>
2019-11-12 21:39:08 +0000

    csai: Huge updates
    - AI works
    - Testing with random # gen
    - Uh, yeah?
    - Bug fixes and AI code revamp
    
    Signed-off-by: nysascape <nysadev@raphielgang.org>

--- AI.cs
+++ AI.cs
@@ -1,72 +1,168 @@
+ 
 using System;
 
-namespace csharpai {
-    class AI {
-        Game game = new Game();
+namespace csharpai
+{
+    class AI
+    {
+        public AI() { }
 
-        string AIPiece;
-        /* So we can make some references to the board class */
-        private string[] copyBoard(string[] boardToCopy) {
-            /* Copy the board so we don't make permament changes */
-            string[] copy = new string[9];
-            for(int i = 0; i < 9; i++) {
-                copy[i] = boardToCopy[i];
+        public int aiTurn(string[] board, string aiToken)
+        {
+            float[] scores = new float[9];
+            for (int i = 0; i < 9; i++)
+            {
+                if (board[i] != " ")
+                {
+                    scores[i] = -1000;
+                    continue;
+                }
+                scores[i] = aiFindRecurse(board, i, aiToken);
             }
-            return boardToCopy;
-        }
-
-        private int minimax(string[] board, string player) {
-            /* The minimax algorithm finds the best spot for the AI to choose. */
-            if(player == "X") {
-                AIPiece = "O";
-            } else {
-                AIPiece = "X";
-            }
-
-            int score = 0;
-            int scoreCount = 0;
-
-            for(int i = 0; i < 9; i++) {
-                    if(board[i] != "") {
-                        continue;
-                    }
-                if(game.checkWin(board, player) == -10) {
-                    return -10;
-                } else {
-                    int highScore = 0;
-                    int bestSpot = 0;
-                    bool firstScore = true;
-                    for(int j = 0; j < 9; j++) {
-                        int aiScore = aiFindRecurse(board, j, AIPiece);
-                        if(firstScore || aiScore > highScore) {
-                            firstScore = false;
-                            highScore = aiScore;
-                            bestSpot = j;
-                        }
-                    }
-                    score += highScore;
-                    scoreCount++;
+            int spot = 0;
+            float highscore = 0;
+            bool firstScore = true;
+            for (int i = 0; i < 9; i++)
+            {
+                if (scores[i] == -1000)
+                {
+                    continue;
+                }
+                if (firstScore || scores[i] > highscore)
+                {
+                    highscore = scores[i];
+                    spot = i;
+                    firstScore = false;
                 }
             }
 
-            int adjScore = 0;
-            if(scoreCount != 0) {
-                adjScore = score / scoreCount;
-            }      
-
-            return adjScore;
+            return spot;
         }
 
-        private int aiFindRecurse(string[] board, int spot, string AI) {
-            board[spot] = AI;
+        private float aiFindRecurse(string[] board, int spot, string aiToken)
+        {
+            string[] boardCopy = copyBoard(board);
 
-            if(game.checkWin(board, AI) == 10) {
+            boardCopy[spot] = aiToken;
+            if (checkWin(boardCopy, aiToken))
+            {
                 return 10;
-            } else if(game.checkGameEnd(board)) {
+            }
+            else if (checkDraw(boardCopy))
+            {
                 return 0;
             }
-            int score = minimax(board, AI);
+
+            float score = fakePlayer(boardCopy, aiToken);
             return score;
+        }
+
+        private float fakePlayer(string[] board, string aiToken)
+        {
+            string playerToken;
+            if (aiToken == "X")
+            {
+                playerToken = "O";
+            }
+            else
+            {
+                playerToken = "X";
+            }
+
+            float score = 0;
+            int scoreCount = 0;
+            for (int i = 0; i < 9; i++)
+            {
+                if (board[i] != " ")
+                {
+                    continue;
+                }
+                string[] boardCopy = copyBoard(board);
+                boardCopy[i] = playerToken;
+                if (checkWin(boardCopy, playerToken))
+                {
+                    return -10;
+                }
+                else
+                {
+                    float highScore = 0;
+                    int bestSpot = 0;
+                    bool firstScore = true;
+                    for (int j = 0; j < 9; j++)
+                    {
+                        if (boardCopy[j] != " ")
+                        {
+                            continue;
+                        }
+                        var aiScore = aiFindRecurse(boardCopy, j, aiToken);
+                        if(firstScore || aiScore>highScore) {
+                            firstScore = false;
+                            highScore = aiScore;
+                            bestSpot=j;
+                        }
+                    }
+                    score+=highScore;
+                    scoreCount++;
+                }
+            }
+    
+            float adjustedScore = 0;
+            if( scoreCount!=0) {
+                adjustedScore = score / scoreCount;
+            }
+
+            return adjustedScore;
+        }
+
+        private bool checkWin(string[] board, string playerToken)
+        {
+            int[,] winConditions = new int[8, 3] {
+                /* These are the combinations that a player can possibly win in a 3x3 grid */
+                { 0, 1, 2 }, { 3, 4, 5 }, { 6, 7, 8 },
+                { 0, 3, 6 }, { 1, 4, 7 }, { 2, 5, 8 },
+                { 0, 4, 8 }, { 2, 4, 6 }
+            };
+            for (int i = 0; i < 8; i++)
+            {
+                int num = 0;
+                for (int j = 0; j < 3; j++)
+                {
+                    if (board[winConditions[i, j]] == playerToken)
+                    {
+                        num++;
+                    }
+                }
+                if (num == 3)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+
+        private bool checkDraw(string[] board)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                if (board[i] == " ")
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private string[] copyBoard(string[] board)
+        {
+            string[] copy = new string[9];
+            for (int i = 0; i < 9; i++)
+            {
+                copy[i] = board[i];
+            }
+
+            return copy;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -15,12 +15,14 @@ namespace csharpai
             /* Generate a new, empty board for a game. */
             string[] board = new string[9];
             for(int i = 0; i < 9; i++) {
-                board[i] = "O";
+                board[i] = " ";
             }
             return board;
         }
 
         public void printBoard(string[] board) {
+            /* Our board printing function. Needed because this
+            game would be pretty boring if you couldn't see the board */
             Console.WriteLine("┌─┬─┬─┐");
             Console.WriteLine("│{0}│{1}│{2}│", board[0], board[1], board[2]);
             Console.WriteLine("├─┼─┼─┤");
@@ -33,8 +35,6 @@ namespace csharpai
 
     class Game
     {
-        static string[] board;
-
         static int[,] winConditions = new int[8, 3] {
             /* These are the combinations that a player can possibly win in a 3x3 grid */
             { 0, 1, 2 }, { 3, 4, 5 }, { 6, 7, 8 },
@@ -44,9 +44,12 @@ namespace csharpai
 
         public static Board gameBoard = new Board();
         /* Initialize a new game */
-        public Game() {
-            board = gameBoard.genBoard();
-            gameBoard.printBoard(board);
+        public Game() { }
+
+        public string[] initGame() {
+            /* initGame just generates a new board, completely clean. 
+            It's like the equivalent of flipping the board in a Monopoly game. */
+            string[] board = gameBoard.genBoard();
             bool loop = true;
 
             /* Verify that the square is either X or O */
@@ -77,14 +80,21 @@ namespace csharpai
 
             System.Console.WriteLine("AI: {0}", gameBoard.AI);
             System.Console.WriteLine("Player: {0}", gameBoard.human);
+            return board;
         }
-
         public void placeMove(string[] board, int move, string player) {
-            /* Just a simple function to place where you want it */
+            /* Just a simple function to place where you want it
+            Pretty vital if the player wants to move. */
             board[move] = player;
         }
 
         public int checkWin(string[] board, string player) {
+            /* checkWin just iterates through possible win conditions
+            in a grid, checking which player has won. 
+            We return numbers so the minimax algorithm can determine
+            which is the best move the AI can make. 
+            Our AI would be useless without these returns. 
+            How would it know? Poor AI. */
             string altplayer;
             if(player == "X") {
                 altplayer = "O";
@@ -109,6 +119,8 @@ namespace csharpai
         }
 
         public bool checkGameEnd(string[] board) {
+            /* checkGameEnd just simply iterates through the board,
+            returns false if the game isn't over (in a draw). */
             foreach(string p in board) {
                 if(p != " ") {
                     return false;
@@ -120,10 +132,36 @@ namespace csharpai
         static void Main(string[] args)
         {
             Game game = new Game();
-            for(int i = 0; i < 9; i++) {
-                board[i] = "X";
+            Board _board = new Board();
+            string[] board = game.initGame();
+
+            AI ai = new AI();
+
+            Random r1 = new Random();
+            while(true) {
+                if(game.checkWin(board, gameBoard.AI) == 0) {
+                } else {
+                    System.Console.WriteLine("Exiting!");
+                    Environment.Exit(0);
+                }
+                int random = r1.Next(0, 9);
+
+                bool loop = true;
+
+                while(loop) {
+                    if(board[random] != " ") {
+                        random = r1.Next(0, 9);
+                    }
+                    else {
+                        loop = !loop;
+                    }
+                }
+
+                game.placeMove(board, random, gameBoard.human); 
+                int result = ai.aiTurn(board, gameBoard.AI);
+                game.placeMove(board, result, gameBoard.AI);
+                _board.printBoard(board);
             }
-            gameBoard.printBoard(board);
         }
     }
 }
